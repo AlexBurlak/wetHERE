74a74f0f72ce7042520ef4be259f6e8528de2cf3
oleksandr-burlakov <83636875+oleksandr-burlakov@users.noreply.github.com>
2021-05-31 13:57:11 +0300

    Removed class library and console application

---
+++ weathere/weather.CL/Models/WeatherDayModel.cs
@@ -0,0 +1,13 @@
+namespace weather.CL.Models
+{
+    public class WeatherDayModel
+    {
+        public double MaxCelsius { get; set; }
+        public double MinCelsius { get; set; }
+
+        public WeatherDayModel()
+        {
+            
+        }
+    }
+}
--- weathere/weather.CL/Models/WheatherModel.cs
+++
@@ -1,7 +0,0 @@
-namespace weather.CL.Models
-{
-    public class WheaterModel
-    {
-        
-    }
-}
